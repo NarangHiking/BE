181894dc5f7881b265d42e15a0326f41a01a3a02
황우찬 <cmy397264@naver.com>
2026-05-18 19:33:08 +0900

    chore:initialize Spring Boot project

---
+++ src/main/java/com/naranghiking/NarangHikingApplication.java
@@ -0,0 +1,12 @@
+package com.naranghiking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NarangHikingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NarangHikingApplication.class, args);
+	}
+}
---
+++ src/test/java/com/naranghiking/NarangHikingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.naranghiking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NarangHikingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
