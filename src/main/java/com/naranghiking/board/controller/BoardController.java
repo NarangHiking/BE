8a6caf2aec123871a40e591f4dec642c4ea49da5
jamonda1 <park00seungjin@gmail.com>
2026-05-24 00:57:29 +0900

    feat: 게시글 전체 조회 로직 수정했습니다. 기존 전체 게시글만 가져오는 로직에서 keyword랑 category를 쿼리로 받아서 값이 있을 때 그것을 통해 조회하는 걸로 변경했습니다.

--- src/main/java/com/naranghiking/board/controller/BoardController.java
+++ src/main/java/com/naranghiking/board/controller/BoardController.java
@@ -9,9 +9,9 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
-import org.apache.coyote.Response;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.parameters.P;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Collections;
@@ -25,25 +25,27 @@ public class BoardController {
 
     private final BoardService boardService;
 
-    @Operation(summary = "게시글 전체 조회", description = "전체 게시글을 조회합니다.")
+    @Operation(summary = "게시글 전체 조회", description = "전체 게시글을 조회합니다. 때로는 각각의 파라미터에 맞춰, 제목, 내용, 카테고리로도 조회가 가능합니다.")
     @ApiResponse(responseCode = "200",description = "조회 성공")
     @GetMapping
-    public ResponseEntity<List<BoardResponse>> list() { // 테스트 확인 완료
-        List<BoardResponse> boards = boardService.selectAll();
+    public ResponseEntity<List<BoardResponse>> list(
+            @Parameter(description = "제목 및 내용 검색", example = "오늘은") @RequestParam(value = "keyword", required = false) String keyword,
+            @Parameter(description = "카테고리로 검색", example = "자유") @RequestParam(value = "category", required = false) String category) {
+        List<BoardResponse> boards = boardService.selectAll(keyword, category); // 키워드랑 카테고리 같이 전달
         // 게시글이 비어있으면 null이 아닌 비어있는 리스트를 반환
         if(boards == null) return ResponseEntity.ok(Collections.emptyList());
         return ResponseEntity.ok(boards);
     }
 
-    @Operation(summary = "게시글 단건 조회", description = "boards의 PK인 id를 통해서 단건의 게시글만 조회합니다.")
+    @Operation(summary = "게시글 상세 조회", description = "boards의 PK인 id를 통해서 단건의 게시글만 조회합니다.")
     @ApiResponses({
             @ApiResponse(responseCode = "200", description = "조회 성공"),
             @ApiResponse(responseCode = "404", description = "조회 실패, 게시글을 찾을 수 없습니다.")
     })
-    @GetMapping("/{id}")
+    @GetMapping("/detail")
     public ResponseEntity<BoardResponse> detail(
             @Parameter(description = "게시글 ID, 단일 게시글 조회에 필요", example = "1")
-            @PathVariable long id) {
+            @RequestParam("id") Long id) {
         BoardResponse board = boardService.selectById(id);
         // 게시글을 찾지 못하면 404 반환
         if(board == null) return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
--- src/main/java/com/naranghiking/board/service/BoardService.java
+++ src/main/java/com/naranghiking/board/service/BoardService.java
@@ -8,9 +8,9 @@ import java.util.List;
 
 public interface BoardService {
     // 전체 게시글 조회
-    List<BoardResponse> selectAll();
+    List<BoardResponse> selectAll(String keyword, String category);
     // 게시글 단건 조회
-    BoardResponse selectById(long id);
+    BoardResponse selectById(Long id);
     // 게시글 생성
     int insert(BoardRequest board);
 }
