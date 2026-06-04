f37feb72ed9ec098ef0870e52a9b9343fbda4318
황우찬 <cmy397264@naver.com>
2026-06-04 14:30:13 +0900

    fix orderNum

--- src/test/java/com/naranghiking/mtn/controller/MtnControllerTest.java
+++ src/test/java/com/naranghiking/mtn/controller/MtnControllerTest.java
@@ -71,7 +71,7 @@ class MtnControllerTest {
                 .andExpect(jsonPath("$.data.name").value("테스트 산"));
     }
     @Test
-    @Order(2)
+    @Order(3)
     @DisplayName("산 추가 - 일반유저")
     void userInsert() throws Exception {
         mockMvc.perform(post("/mtn").header("Authorization", "Bearer " + userToken)
@@ -81,7 +81,7 @@ class MtnControllerTest {
     }
 
     @Test
-    @Order(3)
+    @Order(4)
     @DisplayName("산 단일 조회")
     void select() throws Exception {
         // insert된 산의 id를 가져옴
@@ -97,7 +97,7 @@ class MtnControllerTest {
     }
 
     @Test
-    @Order(4)
+    @Order(5)
     @DisplayName("존재하지 않는 산 단일 조회 - 실패")
     void selectNotFound() throws Exception {
         mockMvc.perform(get("/mtn/999999"))
@@ -105,7 +105,7 @@ class MtnControllerTest {
     }
 
     @Test
-    @Order(5)
+    @Order(6)
     @DisplayName("산 수정")
     void update() throws Exception {
         Mtn inserted = mtnService.selectAll().stream()
@@ -126,7 +126,7 @@ class MtnControllerTest {
     }
 
     @Test
-    @Order(6)
+    @Order(7)
     @DisplayName("산 삭제")
     void mtnDelete() throws Exception {
         Mtn inserted = mtnService.selectAll().stream()
@@ -144,7 +144,7 @@ class MtnControllerTest {
     }
 
     @Test
-    @Order(7)
+    @Order(8)
     @DisplayName("삭제 후 조회 - 실패 확인")
     void selectAfterDelete() throws Exception {
         // 삭제된 산은 더 이상 조회되지 않아야 함
@@ -154,7 +154,7 @@ class MtnControllerTest {
     }
 
     @Test
-    @Order(8)
+    @Order(9)
     @DisplayName("산 하위 경로 표시")
     void selectPathFromMtn() throws Exception {
         mockMvc.perform(get("/mtn/1/track"))
