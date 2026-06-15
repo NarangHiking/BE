3250f976d558487bb5f9919e7ea41eddf98f1779
jamonda1 <park00seungjin@gmail.com>
2026-06-16 01:36:42 +0900

    feat: 챗봇 controller 완성

--- src/main/java/com/naranghiking/chatbot/controller/ChatbotController.java
+++ src/main/java/com/naranghiking/chatbot/controller/ChatbotController.java
@@ -1,7 +1,36 @@
 package com.naranghiking.chatbot.controller;
 
+import com.naranghiking.chatbot.dto.ChatbotRequest;
+import com.naranghiking.chatbot.service.ChatbotService;
+import com.naranghiking.common.dto.ApiResult;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Tag(name = "챗봇에게 질문을 하고, 답변을 받는 컨트롤러")
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/chat")
 public class ChatbotController {
-    // redis + pinecone 조합으로 진행
-    // redis를 통해서 사용자의 대화를 단기로 기억하고, pinecone을 통해서 vector DB 사용.
-    // controller -> service -> Spring Ai 라이브러리 -> Pinecone & openAI로 진행
+
+    private final ChatbotService chatbotService;
+
+    @Operation(summary = "질문 요청 처리", description = "질문 후 RAG 기반 챗봇이 도출한 답변을 반환")
+    @PostMapping
+    public ResponseEntity<ApiResult<String>> chat(
+            @Parameter(description = "사용자의 id, redis에서 추출", example = "1")
+            @AuthenticationPrincipal Long userId,
+            @Parameter(description = "사용자의 질문", example = "가볍게 즐기기 좋은 한라산 코스 추천해줘")
+            @Valid @RequestBody ChatbotRequest request) {
+        String result = chatbotService.chat(userId.toString(), request.message());
+        return ResponseEntity.ok(ApiResult.success(result));
+    }
 }
