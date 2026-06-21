6188b697cc37534d670fe0f5cea4daffdd211d98
황우찬 <cmy397264@naver.com>
2026-06-22 07:51:08 +0900

    fix cors

--- src/main/java/com/naranghiking/common/config/SecurityConfig.java
+++ src/main/java/com/naranghiking/common/config/SecurityConfig.java
@@ -90,6 +90,7 @@ public class SecurityConfig {
         return http.build();
     }
 
+    //
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
