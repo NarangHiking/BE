37979f6237b0b900e067137f7a9e4337b9ef1b4b
jamonda1 <park00seungjin@gmail.com>
2026-06-23 01:31:49 +0900

    feat: LLM 체이닝을 위한 리팩토링 진행 중

--- src/main/java/com/naranghiking/chatbot/dto/ChatbotRequest.java
+++ src/main/java/com/naranghiking/chatbot/dto/ChatbotRequest.java
@@ -1,15 +1,15 @@
 package com.naranghiking.chatbot.dto;
 
 import io.swagger.v3.oas.annotations.media.Schema;
-import jakarta.annotation.Nullable;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
 @Schema(description = "챗봇에게 전달하기 위한 사용자 질문 DTO")
 public record ChatbotRequest(
 
         @Schema(description = "사용자가 보고 있는 산의 ID", example = "1")
-        @Nullable // 일단은 nullable로 진행
+        @NotNull(message = "무조건 산 ID가 전달되어야 함")
         Long mountainId,
 
         @Schema(description = "사용자의 질문", example = "가볍게 즐기기 좋은 한라산 코스 추천해줘")
