7e616cb0ce2a11940f7ab7ebc9f87cd63f2f7608
황우찬 <cmy397264@naver.com>
2026-06-23 13:47:25 +0900

    remove MtnGrid

--- src/main/java/com/naranghiking/weather/service/WeatherService.java
+++ src/main/java/com/naranghiking/weather/service/WeatherService.java
@@ -1,7 +1,6 @@
 package com.naranghiking.weather.service;
 
 import com.naranghiking.weather.dto.KmaApiResponse;
-import com.naranghiking.weather.dto.MtnGrid;
 import com.naranghiking.weather.dto.WeatherResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -35,9 +34,9 @@ public class WeatherService {
 
     private static final DateTimeFormatter DATE_FMT = DateTimeFormatter.ofPattern("yyyyMMdd");
 
-    public List<WeatherResponse> getWeather(String mtnName, String fcstDate) {
-        MtnGrid grid = MtnGrid.fromName(mtnName);
-        String [] base = resolveBaseDateTime();
+    public List<WeatherResponse> getWeather(double lat, double lng, String fcstDate) {
+        int[] grid = latLngToGrid(lat, lng);
+        String[] base = resolveBaseDateTime();
 
         URI uri = UriComponentsBuilder.fromUriString(BASE_URL)
                 .queryParam("serviceKey", apiKey)
@@ -46,8 +45,8 @@ public class WeatherService {
                 .queryParam("dataType", "JSON")
                 .queryParam("base_date", base[0])
                 .queryParam("base_time", base[1])
-                .queryParam("nx", grid.getNx())
-                .queryParam("ny", grid.getNy())
+                .queryParam("nx", grid[0])
+                .queryParam("ny", grid[1])
                 .build(true)
                 .toUri();
 
@@ -106,5 +105,31 @@ public class WeatherService {
         String baseDate = now.format(DATE_FMT);
         String baseTimeStr = String.format("%02d00", baseTime);
         return new String [] {baseDate, baseTimeStr};
+    }
+
+    private int[] latLngToGrid(double lat, double lng) {
+        double RE = 6371.00877, GRID = 5.0;
+        double SLAT1 = 30.0, SLAT2 = 60.0, OLON = 126.0, OLAT = 38.0;
+        double XO = 43, YO = 136;
+        double DEGRAD = Math.PI / 180.0;
+
+        double re = RE / GRID;
+        double slat1 = SLAT1 * DEGRAD, slat2 = SLAT2 * DEGRAD;
+        double olon = OLON * DEGRAD, olat = OLAT * DEGRAD;
+
+        double sn = Math.log(Math.cos(slat1) / Math.cos(slat2))
+                  / Math.log(Math.tan(Math.PI * 0.25 + slat2 * 0.5) / Math.tan(Math.PI * 0.25 + slat1 * 0.5));
+        double sf = Math.pow(Math.tan(Math.PI * 0.25 + slat1 * 0.5), sn) * Math.cos(slat1) / sn;
+        double ro = re * sf / Math.pow(Math.tan(Math.PI * 0.25 + olat * 0.5), sn);
+
+        double ra = re * sf / Math.pow(Math.tan(Math.PI * 0.25 + lat * DEGRAD * 0.5), sn);
+        double theta = lng * DEGRAD - olon;
+        if (theta > Math.PI) theta -= 2.0 * Math.PI;
+        if (theta < -Math.PI) theta += 2.0 * Math.PI;
+        theta *= sn;
+
+        int nx = (int) Math.floor(ra * Math.sin(theta) + XO + 0.5);
+        int ny = (int) Math.floor(ro - ra * Math.cos(theta) + YO + 0.5);
+        return new int[]{nx, ny};
     }
 }
